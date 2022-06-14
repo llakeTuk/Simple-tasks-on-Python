ef7428959fdf485d0042ee04c7fd9a3801907da2
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-14 20:16:34 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,22 @@
+A = 0
+B = 0
+Operator = ''
+def calculation(A, B, Operator):
+    A = int(input("Input value of A: "))
+    B = int(input("Input value of B: "))
+    Operator = input("Input Operator ")
+    if Operator == '+':
+        return print(A + B)
+    elif Operator == '-':
+        return print(A - B)
+    elif Operator == '*':
+        return print(A * B)
+    elif Operator =='/':
+        return print(A / B)
+    else:
+        return print('Unknown operator, only use +-*/')
+i = 'y'
+while i == 'y':
+    calculation(A, B, Operator)
+    i = input("Would you like to start new calculation?(y/n) ")
+print("Goodbye")
