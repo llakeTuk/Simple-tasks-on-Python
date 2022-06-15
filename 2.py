709bf8d614a0a9c8ce7a005af8079b09b073ea34
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-15 20:25:53 +0300

    Add files via upload
    Leap year finder

---
+++ 2.py
@@ -0,0 +1,16 @@
+import math
+A = 0
+
+def leap(A):
+    A = int(input("Input year to check: "))
+    if A % 4 == 0 and A % 100 != 0:
+        print("Year is leap")
+    elif A % 400 == 0:
+        print("Year is leap")
+    else:
+        print("Year is not leap")     
+i = 'y'
+while i == 'y':
+    leap(A)
+    i = input("Would you like to start new check?(y/n) ")
+print("Goodbye")
