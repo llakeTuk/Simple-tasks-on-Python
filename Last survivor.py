9b2688ca4fd63155c35453dd3bcb9a0dfe18433a
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-19 18:31:49 +0300

    Add files via upload
    Program returns number of last survivor from group of people

---
+++ Last survivor.py
@@ -0,0 +1,5 @@
+a, b = int(input("enter number of people: ", )), int(input("enter count number: ", ))
+survivor = 0
+for i in range(1, a + 1):
+  survivor = (survivor + b) % i
+print("survivor is number: ", survivor + 1)
