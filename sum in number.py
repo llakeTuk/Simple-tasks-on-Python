aff583a83ccb6c81b98e045dcc031199e0b9f7f8
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-25 11:47:17 +0300

    Add files via upload
    Returns sum of digits in number

---
+++ sum in number.py
@@ -0,0 +1,7 @@
+y = 'y'
+while y == 'y':
+  number = input('enter number: ')
+  digits = [int(i) for i in str(number)]
+  print('digits in number: ', *digits)
+  print('sum of digits: ', sum(digits))
+  y = input('restart?(y/n): ')
