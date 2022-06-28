210412d9f83d0ce66aa8bef9ab04fc279c694d31
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-28 15:19:39 +0300

    Update Last survivor.py

--- Last survivor.py
+++ Last survivor.py
@@ -1,5 +1,9 @@
-a, b = int(input("enter number of people: ", )), int(input("enter count number: ", ))
-survivor = 0
-for i in range(1, a + 1):
-  survivor = (survivor + b) % i
-print("survivor is number: ", survivor + 1)
+y = 'y'
+while y == 'y':
+  a, b = int(input("enter number of people: ", )), int(input("enter count number: ", ))
+  survivor = 0
+  for i in range(1, a + 1):
+    survivor = (survivor + b) % i
+  print("survivor is number: ", survivor + 1)
+  y = input("restart?(y/n): ", )
+Print("goodbye")
